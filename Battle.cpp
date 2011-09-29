a0b930ba9d59ce1f000f4ca477eadd49950da04b
Chris Miller <lordsauronthegreat@gmail.com>
2011-09-29 14:34:18 -0600

    Went totally OCD on the formatting. Also, loops!

--- Battle.cpp
+++ Battle.cpp
@@ -1,74 +1,76 @@
+//Results of Battle
+// 
+//1 = Atk Wins both rolls 
+//2 = 1 loss on both sides 
+// 3 = Def wins two 
+// 4 = Atk wins 1 
+//5 Def wins 1
 
-//Results of Battle// //1 = Atk Wins both rolls //2 = 1 loss on both sides // 3 = Def wins two // 4 = Atk wins 1 //5 Def wins 1
-
-#include "battle.h"
-#include "DiceRoll.h"
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
+#include "battle.h"
+#include "DiceRoll.h"
+
 using namespace std;
 
-Battle::Battle(){
+Battle::Battle() {
 
 }
 
-
-int Battle::Results(std::vector<int>& Atkroll, std::vector<int>& Defroll){
-
-                if (Atkroll.size() == 3){
-                    if (Defroll.size() == 2){
-                        if (Atkroll[2] > Defroll[1]){
-                            if (Atkroll[1] > Defroll[0])
-                                return 1;
-                            else
-                                return 2;}
-                        else if (Atkroll[2] <= Defroll[1]){
-                            if (Atkroll[1] <= Defroll[0])
-                                return 3;
-                            else
-                                return 2;}}
-
-                    else if (Defroll.size() == 1){
-                           if (Atkroll[2] > Defroll[0])
-                                    return 4;
-                            else
-                                    return 5;}}
-
-                if (Atkroll.size() == 2){
-                    if (Defroll.size() == 2)
-                        if (Atkroll[1] > Defroll[1]){
-                            if (Atkroll[0] > Defroll[0])
-                                return 1;
-                            else
-                                return 2;}
-                        else if (Atkroll[1] <= Defroll[1]){
-                            if (Atkroll[0] <= Defroll[0])
-                                return 3;
-                            else
-                                return 2;}
-
-                    else if (Defroll.size() == 1){
-                           if (Atkroll[1] > Defroll[0])
-                                    return 4;
-                            else
-                                    return 5;}}
-
-
-                if (Atkroll.size() == 1){
-                    if (Defroll.size() == 2)
-                        if (Atkroll[0] > Defroll[1])
-                                return 4;
-                        else
-                             return 5;
-
-                    else if (Defroll.size() == 1){
-                        if (Atkroll[0] > Defroll[0])
-                                return 4;
-                        else
-                             return 5;}}
-
+int Battle::Results(std::vector<int>& Atkroll, std::vector<int>& Defroll) {
+    if (Atkroll.size() == 3) {
+        if (Defroll.size() == 2) {
+            if (Atkroll[2] > Defroll[1]) {
+                if (Atkroll[1] > Defroll[0])
+                    return 1;
+                else
+                    return 2;
+            } else if (Atkroll[2] <= Defroll[1]) {
+                if (Atkroll[1] <= Defroll[0])
+                    return 3;
+                else
+                    return 2;
+            }
+        } else if (Defroll.size() == 1) {
+            if (Atkroll[2] > Defroll[0])
+                return 4;
+            else
+                return 5;
+        }
+    }
+    if (Atkroll.size() == 2) {
+        if (Defroll.size() == 2)
+        if (Atkroll[1] > Defroll[1]) {
+            if (Atkroll[0] > Defroll[0])
+                return 1;
+            else
+                return 2;
+        } else if (Atkroll[1] <= Defroll[1]) {
+            if (Atkroll[0] <= Defroll[0])
+                return 3;
+            else
+                return 2;
+        } else if (Defroll.size() == 1) {
+            if (Atkroll[1] > Defroll[0])
+                return 4;
+            else
+                return 5;
+        }
+    }
+    if (Atkroll.size() == 1) {
+        if (Defroll.size() == 2)
+            if (Atkroll[0] > Defroll[1])
+                return 4;
+            else
+                return 5;
+        else if (Defroll.size() == 1) {
+            if (Atkroll[0] > Defroll[0])
+                return 4;
+            else
+                return 5;
+        }
+    }
     return 0;
 }
-
-
-
--- DiceRoll.cpp
+++ DiceRoll.cpp
@@ -1,99 +1,76 @@
+
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <cstdlib>
 #include <time.h>
 #include <vector>
+
 #include "DiceRoll.h"
 #include "Sorter.h"
+
 using namespace std;
+
+ostream& operator<< (ostream& out, vector<int>& vec){
+    for (vector<int>::iterator iter = vec.begin(); iter != vec.end(); iter++)
+        out << " " << *iter;
+    return out;
+}
 
 Sorter mySorter;
 
+DiceRoll::DiceRoll() {
 
-DiceRoll::DiceRoll(){
 }
 
-namespace atk
-        {
-            int roll;
+namespace atk {
+    int roll;
+}
 
-        }
+namespace def {
+    double roll;
+}
 
-namespace def
-        {
-            double roll;
+int DiceRoll::GetDefArmies(int D) {
+    if (D > 1)
+        def::roll = 2;
+    else if (D == 1)
+        def::roll = 1;
+    return def::roll;
+}
 
-
-        }
-
-
- int DiceRoll::GetDefArmies(int D){
-
-        if (D > 1){
-            def::roll = 2;}
-        else if (D == 1){
-            def::roll = 1;}
-
-return def::roll;
-    }
-
-  int DiceRoll::GetAtkArmies(int A){
-
-  retry:    cout << "How Many Units is the Attacker using?: ";
-            cin >> atk::roll;
-
-        if (atk::roll >= A){
-            if ((atk::roll - A) <= 0){
-                cout << "Sorry, there needs to be at least one left. Try Again." << endl;
-                    goto retry;}
-            else
-                cout << "Sorry you do not have enough. Try Again " << endl;
-                    goto retry;}
-
-
+int DiceRoll::GetAtkArmies(int A) {
+    do {
+        cout << "How Many Units is the Attacker using?: ";
+        cin >> atk::roll;
+        
+        if (atk::roll >= A) {
+            if ((atk::roll - A) <= 0) {
+                cout << "Sorry, there needs to be at least one left. Try again." << endl;
+                continue;
+            } else {
+                cout << "Sorry, you do not have enough. Try Again " << endl;
+                continue;
+            }
+        } else
+            break;
+    } while (true);
+    
     return atk::roll;
 }
 
 
-int DiceRoll::Roll(int atk, int def){
-                vector<int> aroll;
-                vector<int> droll;
-                    srand (time(NULL));
+int DiceRoll::Roll(int atk, int def) {
+    vector<int> aroll;
+    vector<int> droll;
+    srand (time(NULL));
+    for (size_t i = 0; i < atk; ++i)
+        aroll.push_back ((rand() % 6)+1);
+    cout << "Attacker Rolls: " << aroll << endl;
+    
+    for (size_t i = 0; i < def; ++i)
+        droll.push_back ((rand() % 6)+1);
+    count << "Defenders Rolls: " << droll << endl;
 
-                        if (atk >= 3){
-                                aroll.push_back ((rand() % 6)+1);
-                                aroll.push_back ((rand() % 6)+1);
-                                aroll.push_back ((rand() % 6)+1);
-                                    cout << "Attacker Rolls: " <<  aroll[0] << " " << aroll[1] << " " << aroll[2] << endl;
-                                }
-
-                        if (atk == 2){
-                                aroll.push_back ((rand() % 6)+1);
-                                aroll.push_back ((rand() % 6)+1);
-                                    cout << "Attacker Rolls: " << aroll[0] << " " << aroll[1] << endl;
-                                        }
-
-                        if (atk == 1){
-                                aroll.push_back ((rand() % 6)+1);
-                                    cout << "Attacker Rolls: " << aroll[0] << endl;
-                                        }
-
-
-                        if (def >= 2) {
-                                droll.push_back ((rand() % 6)+1);
-                                droll.push_back ((rand() % 6)+1);
-                                    cout << " Defenders Rolls: " << droll[0] << " " << droll[1] << endl;
-                            }
-
-
-                        if (def == 1){
-                                droll.push_back ((rand() % 6)+1);
-                                    cout << " Defenders Rolls: " << droll[0] << endl;
-                            }
-
-
-
-                    int i = mySorter.asorter(aroll, droll);
- return i;
+    return mySorter.asorter(aroll, droll);
 }
